b1cd22b678787abb66eb8d9591698cee5bd78757
Aldo Hennink <a.hennink@tudelft.nl>
2018-04-03 21:40:52 +0200

    use intrinsic 'iso_fortran_env' module for real kind

--- src/FortranParser_parameters.f90
+++ src/FortranParser_parameters.f90
@@ -8,8 +8,10 @@ MODULE FortranParser_parameters
   !--------- -------- --------- --------- --------- --------- --------- --------- -----
   ! Specify data types
   !--------- -------- --------- --------- --------- --------- --------- --------- -----
+  use, intrinsic :: iso_fortran_env, only: REAL64
   IMPLICIT NONE
-  INTEGER, PARAMETER :: rn = selected_real_kind(p=6,r=37)          ! Precision of real numbers
+  
+  INTEGER, PARAMETER :: rn = REAL64               ! Precision of real numbers
   INTEGER, PARAMETER :: is = SELECTED_INT_KIND(1) ! Data type of bytecode
 END MODULE FortranParser_parameters
 
